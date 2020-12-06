45db87528c541b3e41d3dc9b4456ce994bf0a4e8
wintanaa <winta.abera@ucalgary.ca>
2020-12-05 21:35:31 -0700

    removed console.log

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 function formatDate(timestamp) {
-  //calculate the date
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
@@ -23,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
